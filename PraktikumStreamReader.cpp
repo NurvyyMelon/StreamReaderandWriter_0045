09082bc90b2ce158e5904eeb6b16cee44d0162ac
NurvyyMelon <adinnaufal9@gmail.com>
2026-06-01 12:04:37 +0700

    Menambahkan kelas TokoElektronik dengan atribut etalase dan konstruktor

--- PraktikumStreamReader.cpp
+++ PraktikumStreamReader.cpp
@@ -7,3 +7,18 @@
 #include <limits>
 
 using namespace std;
+
+// Konstanta untuk nama file database
+const string NAMA_FILE = "gudang.txt";
+
+class TokoElektronik {
+private:
+    // Atribut private berupa array kapasitas 3 elemen untuk representasi etalase
+    array<string, 3> etalase;
+
+public:
+    TokoElektronik() {
+        etalase[0] = "Laptop Lenovo LOQ";
+        etalase[1] = "Laptop HP Victus";
+        etalase[2] = "Smartphone Poco F7 Pro";
+    }
